40f26a7783ffefac860dfc16389818f51b2fd23f
TBthe1 <bjorntruye@hotmail.com>
2020-08-17 22:40:37 +0200

    init

---
+++ dist/js/main.js
@@ -0,0 +1,19 @@
+const name = 'Brad';
+console.log('OUTPUT: Brad', Brad);
+
+function name() {
+  console.log(test);
+}
+
+console.log('test');
+console.error('error');
+
+const add = (num1, num2) => {
+  console.log('num1 + num2');
+};
+
+add(1, 2);
+
+const arr = [1, 2, 3, 4, 5];
+
+arr.forEach((num) => console.log(num));
